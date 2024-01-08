f2db108ffc9b8f8a12815fcf77bedced30522d34
Phu Le <21522466@gm.uit.edu.vn>
2024-01-08 16:09:37 +0700

    [CustomerAPI] fix get-all-customers bug

--- RetroFootballAPI/Services/CustomerRepo.cs
+++ RetroFootballAPI/Services/CustomerRepo.cs
@@ -72,7 +72,7 @@ namespace RetroFootballAPI.Services
         public async Task<IEnumerable<Customer>> GetAll()
         {
             return await _context.Customers
-                .Where(c => c.ID.ToLower().IndexOf("cs") == 0)
+                .Where(c => c.ID.ToLower().IndexOf("cs") >= 0)
                 .ToListAsync();
         }
 
